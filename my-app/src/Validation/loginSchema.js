ceeda6f71b0a7a93d793dc25cb65021cdb6b8447
emilme <emilme@sir.mle>
2020-06-24 11:48:07 -0700

    merging

--- my-app/src/Validation/loginSchema.js
+++ my-app/src/Validation/loginSchema.js
@@ -6,6 +6,7 @@ const loginSchema = yup.object().shape({
         .required("Must include a usename.")
         .max(255, "No more than 255 characters"),
     password: yup
+        .string()
         .required("Must include your password")
         .max(255, "No more than 255 characters"),
 })
