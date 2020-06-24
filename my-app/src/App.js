91e8efc6c4c81ea559ede2eb43a7ac854c51cfc7
yirano <yirano@protonmail.ch>
2020-06-24 14:45:13 -0400

    Deploys Correctly

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
 import axios from 'axios'
 import * as Yup from 'yup'
 
 //imported components
 import Nav from './Components/Nav'
 import Login from './Components/Login Page/LoginForm'
-import PhotoGrid from './Components/Photo Grid/PhotoGrid';
+import PhotoGrid from './Components/Photo Grid/PhotoGrid'
 import Post from './Components/View Post/Post'
-import CreatePostForm from './Components/Create Post/CreatePostForm';
-import Register from './Components/Login Page/Register';
-import SampleLogin from './Components/Login Page/SampleLogin'; 
-import UpdateForm from './Components/Create Post/UpdateForm';
+import CreatePostForm from './Components/Create Post/CreatePostForm'
+import Register from './Components/Login Page/Register'
+import SampleLogin from './Components/Login Page/SampleLogin'
+import UpdateForm from './Components/Create Post/UpdateForm'
 //Schemas
 import loginSchema from './Validation/loginSchema'
 
@@ -26,96 +26,96 @@ const intialErrors = {
   password: '',
 }
 
-const intialUser = [];
+const intialUser = []
 const initialDisabled = true
 
 function App() {
 
-    //setStates//
-    const [user, setuser] = useState(intialUser)
-    const [formValues, setFormValues] = useState(intialValues)
-    const [error, setError] = useState(intialErrors)
-    const [disabled, setDisabled] = useState(initialDisabled)       
-    
-      ///basic form stuff////////////////////
-      const getNewuser = ()=>{
-        axios.get('https://reqres.in/api/users', formValues)
-        .then(res=>{
-          setuser([...user, res.data])
-        })
-        .catch(err=>{
-          console.log(err)
-          debugger
-        })
-      };
-    
-      const postNewuser = (newuser)=>{
-        axios.post('https://reqres.in/api/users', newuser)
-        .then(res=>{
-          setuser([...user, res.data])
-          console.log(res.data)
-        })
-        .catch(err=>{
-          console.log(err)
-          debugger
-        })
-        .finally(() => {
-          setFormValues(intialValues)
-          console.log('hey')
-        })
-      }
-    
-      const onInputChange = evt =>{
-        const{name, value} = evt.target;
-    
-        Yup
-          .reach(loginSchema, name)
-          .validate(value)
-          .then(
-            setError({
-            ...error,
-            [name]:''
-            }))
-          .catch(err=>{
-            setError({
-              ...error,
-              [name]: err.errors[0]
-            })
-          })
-    
-        setFormValues({
-          ...formValues,
-          [name]: value,
-        })
-      }
-    
-      const onSubmit = evt =>{
-        evt.preventDefault()
-    
-        const newuser = {
-          username: formValues.username,
-          password: formValues.password
-        }
-    
-        postNewuser(newuser)
-      }
+  //setStates//
+  const [user, setuser] = useState(intialUser)
+  const [formValues, setFormValues] = useState(intialValues)
+  const [error, setError] = useState(intialErrors)
+  const [disabled, setDisabled] = useState(initialDisabled)
 
-      useEffect(()=>{
-        getNewuser()
-      },[])
-  
-      useEffect(() => {
-        loginSchema.isValid(formValues).then(valid => {
-          console.log(valid)
-          setDisabled(!valid)
+  ///basic form stuff////////////////////
+  const getNewuser = () => {
+    axios.get('https://reqres.in/api/users', formValues)
+      .then(res => {
+        setuser([...user, res.data])
+      })
+      .catch(err => {
+        console.log(err)
+        debugger
+      })
+  }
+
+  const postNewuser = (newuser) => {
+    axios.post('https://reqres.in/api/users', newuser)
+      .then(res => {
+        setuser([...user, res.data])
+        console.log(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+        debugger
+      })
+      .finally(() => {
+        setFormValues(intialValues)
+        console.log('hey')
+      })
+  }
+
+  const onInputChange = evt => {
+    const { name, value } = evt.target
+
+    Yup
+      .reach(loginSchema, name)
+      .validate(value)
+      .then(
+        setError({
+          ...error,
+          [name]: ''
+        }))
+      .catch(err => {
+        setError({
+          ...error,
+          [name]: err.errors[0]
         })
-      }, [formValues])
+      })
+
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    })
+  }
+
+  const onSubmit = evt => {
+    evt.preventDefault()
+
+    const newuser = {
+      username: formValues.username,
+      password: formValues.password
+    }
+
+    postNewuser(newuser)
+  }
+
+  useEffect(() => {
+    getNewuser()
+  }, [])
+
+  useEffect(() => {
+    loginSchema.isValid(formValues).then(valid => {
+      console.log(valid)
+      setDisabled(!valid)
+    })
+  }, [formValues])
   return (
     <div className="App">
       <Nav />
       <PhotoGrid />
       <Post />
-      <SampleLogin/>
+      <SampleLogin />
       <Login
         value={formValues}
         onInputChange={onInputChange}
@@ -134,10 +134,10 @@ function App() {
         onSubmit={onSubmit}
         disabled={disabled}
       />
-      
+
 
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- my-app/src/Validation/loginSchema.js
+++ my-app/src/Validation/loginSchema.js
@@ -1,13 +1,14 @@
-import * as yup from "yup";
+import * as yup from "yup"
 
 const loginSchema = yup.object().shape({
-    username: yup
-        .string()
-        .required("Must include a usename.")
-        .max(255, "No more than 255 characters"),
-    password: yup
-        .required("Must include your password")
-        .max(255, "No more than 255 characters"),
+  username: yup
+    .string()
+    .required("Must include a usename.")
+    .max(255, "No more than 255 characters"),
+  password: yup
+    .string()
+    .required("Must include your password")
+    .max(255, "No more than 255 characters"),
 })
 
-export default loginSchema;
+export default loginSchema
