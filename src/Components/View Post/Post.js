284e7111d7aa2db4cad4b2272fc9f762bd8eb7a9
karina-rodriguez <karminer60@gmail.com>
2020-06-25 18:02:55 -0700

    added hard coded user ids to login form

--- src/Components/View Post/Post.js
+++ src/Components/View Post/Post.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Card, CardBody, CardTitle, CardText, CardImg } from 'reactstrap';
+import { Link } from "react-router-dom";
+
 
 function Post(props) {
   const {details} = props
 
   return (
+    //<Link to={`/posts/${details.id}`}>}
       <div className='card'>
+
             <Card >
                 <CardImg top width="100%" src={details.photo_url} alt={details.title}/>
                 <CardBody>
@@ -14,6 +18,7 @@ function Post(props) {
                 </CardBody>
             </Card>
     </div>
+    //</Link>}
   )
 } 
 
